edc37fb05a6907384e7e7b8ae6dbaf4e126ced6a
aa096 <126397865+aa096@users.noreply.github.com>
2024-10-02 20:57:31 +0200

    installs

---
+++ src/main.jsx
@@ -0,0 +1,15 @@
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+import Theme from './styles/theme.jsx'
+import GlobalStyle from './styles/globalStyle.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <Theme>
+      <GlobalStyle/>
+      <App />
+    </Theme>
+  </StrictMode>,
+)
